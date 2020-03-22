0ecc39f03a34dca8b1e8dc6e80a54dac9c302784
محمود ابراهيم <106960182@omaninfo.gov.om>
2020-03-22 17:22:42 +0400

    Pandas Exercise

---
+++ pandas.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+#Series
+labels = ['a','b','c']
+my_data = [10,20,30]
+
+print(pd.Series(data = my_data))
---
+++ pandasFile.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+#Series
+numbers = {1,2,3,4,5}
+ser = pd.Series(list(numbers))
